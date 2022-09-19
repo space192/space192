9c17163b43c89a4634e5c4573e25202342c55f97
Arthur Fournier <44359684+space192@users.noreply.github.com>
2022-09-19 15:05:01 +0200

    Update install_ros_galactic.sh

--- install_ros_galactic.sh
+++ install_ros_galactic.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt update && sudo apt install curl gnupg lsb-release -y
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu focal main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 sudo apt update
 sudo apt upgrade -y
 sudo apt install ros-galactic-desktop -y
